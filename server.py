66fff4605eaf48f8aff6a3cfdc37e7f05bb9b9a9
HelenaMDev <hmoreraweb@gmail.com>
2024-12-12 11:11:16 -0500

    Fixed code according to static code analysis by pylint

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
-from flask import Flask, request, render_template 
+"""
+Emotion Detection Flask app
+Uses Watson's AI API to analyze the emotion of a text 
+"""
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Render index page
+    """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def detector():
+    """ 
+    Analyze the emotion of a text passed by GET request
+    """
     text_to_analyze = request.args.get("textToAnalyze")
 
     # Get the analysis
@@ -26,7 +36,8 @@ def detector():
         if result != "":
             result += ","
         result += f" '{emotion}': {score}"
-    result = f"For the given statement, the system response is {result}. The dominant emotion is <b>{dominant_emotion}</b>"
+    result = f"For the given statement, the system response is {result}."
+    result += f"The dominant emotion is <b>{dominant_emotion}</b>"
     return result
 
 if __name__ == "__main__":
