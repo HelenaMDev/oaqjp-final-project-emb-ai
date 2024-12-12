575afab8635651a0dcc891f54ce1d4a17e9e0139
HelenaMDev <hmoreraweb@gmail.com>
2024-12-12 08:20:59 -0500

    Modified emotion_detector() to return a dictionary of emotions and the dominant emotion name

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,14 +2,31 @@ import requests, json
 
 def emotion_detector(text_to_analyze):
     '''
-    Sends text_to_analyze to Watson AI's API and returnst the result as text
+    Sends text_to_analyze to Watson AI's API and formats the result
     '''
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
 
-    # Send the request
+    # Send the request to Watson's AI API
     response = requests.post(url, json = myobj, headers=headers, timeout=60)
 
-    # Return its text property
-    return response.text
+    # Format the response as JSON dictionary
+    formatted_response = json.loads(response.text)
+
+    # Extract the emotions dictionary
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    # Find the dominant emotion
+    dominant_emotion = ""
+    max_score = 0
+    for emotion, score in emotions.items():
+        if score > max_score:
+            dominant_emotion = emotion
+            max_score = score
+
+    # Add the dominant emotion to the dictionary
+    emotions['dominant_emotion'] = dominant_emotion
+
+    # Return the emotions dictionary
+    return emotions
